b1287423643ce7a8d3d03e864cae99c467b30fe2
level1 <level1@localhost.localdomain>
2018-04-01 22:59:12 -0600

    revised msr2.sh; previously the value assignment wasn't correct;

--- src/msr2.sh
+++ src/msr2.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-for p in `cat "msr-index.h" | grep define | grep MSR | grep 0x | awk -F ' ' '{print $3}'`
-do echo "$p"
-rdmsr -p0 $p
+for p in `cat "msr-index.h" | grep define | grep MSR | grep 0x | grep -v fffff | awk {'printf ("%s%s\n", $2, $3)'} `
+do
+msr=`echo $p | awk -F '0x' '{print $1}' | tr -d "\n"`
+echo "$msr"
+value=`echo $p | awk -F '0x' '{print $2}' | tr -d "\n"`
+value="0x${value}"
+#rdmsr -p0 $value
 done
 
-for p in `cat "hyperv.h" | grep define | grep MSR | grep 0x | awk -F ' ' '{print $3}'`
-do echo "$p"
-rdmsr -p0 $p
+for p in `cat "hyperv.h" | grep define | grep MSR | grep 0x | grep -v fffff | awk {'printf ("%s%s\n", $2, $3)'} `
+do
+msr=`echo $p | awk -F '0x' '{print $1}' | tr -d "\n"`
+echo "$msr"
+value=`echo $p | awk -F '0x' '{print $2}' | tr -d "\n"`
+value="0x${value}"
+#rdmsr -p0 $value
 done
 
-wrmsr -p 0 0x00000186 0x004300c0
-wrmsr -p 0 0x10 0
+#wrmsr -p 0 0x00000186 0x004300c0
+#wrmsr -p 0 0x10 0
 
